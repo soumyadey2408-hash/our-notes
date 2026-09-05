c86bae7b3a20897d47e2e345818716f0235047ba
soumyadey2408-hash <soumyadey2408@gmail.com>
2026-09-05 14:35:05 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFCA1RHz6SwklOCekNew1S7fB3L7pzqU0",
+  authDomain: "aurora-notes-d2fda.firebaseapp.com",
+  projectId: "aurora-notes-d2fda",
+  storageBucket: "aurora-notes-d2fda.firebasestorage.app",
+  messagingSenderId: "25351995380",
+  appId: "1:25351995380:web:88ef32a81d420e5627c3a1"
+};
